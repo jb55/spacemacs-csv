8b93635ed0127a2ce12cbbb9f51ff44d4b0f70f6
William Casarin <bill@casarin.me>
2015-11-26 13:59:53 -0800

    remove cruft

--- packages.el
+++ packages.el
@@ -11,20 +11,26 @@
 ;;; License: GPLv3
 
 (defvar csv-packages
-  '(
+  '( csv-mode
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
-(defvar csv-excluded-packages '()
-  "List of packages to exclude.")
-
-;; For each package, define a function csv/init-<package-csv>
-;;
-;; (defun csv/init-my-package ()
-;;   "Initialize my package"
-;;   )
-;;
-;; Often the body of an initialize function uses `use-package'
-;; For more info on `use-package', see readme:
-;; https://github.com/jwiegley/use-package
+(defun csv/init-csv-mode ()
+  "Initial csv mode"
+  (use-package csv-mode
+    :init
+    (evil-leader/set-key-for-mode 'csv-mode
+      "mt"  'csv-transpose
+      "ma"  'csv-align-fields
+      "mu"  'csv-unalign-fields
+      "msf" 'csv-sort-fields
+      "msn" 'csv-sort-numeric-fields
+      "mso" 'csv-toggle-descending
+      "mn"  'csv-forward-field
+      "mp"  'csv-backward-field
+      "mr"  'csv-reverse-region
+      "md"  'csv-kill-fields
+      "mi"  'csv-toggle-invisibility
+      "mvf" 'csv-yank-fields
+      "mvt" 'csv-yank-as-new-table)))
